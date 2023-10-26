7be9a4bd5310f95197feb4af1828637a0fadf705
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-10-26 14:05:56 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,24 @@
+
+const parent = React.createElement("div",
+    { id: "parent" },
+    [
+        React.createElement("div", { id: "child" }, [
+            React.createElement("h1", {}, "I am a H1 tag"),
+            React.createElement("h2", {}, "I am a h2 tag")
+        ]),
+        React.createElement("div", { id: "child2" }, [
+            React.createElement("h1", {}, "I am a H1 tag"),
+            React.createElement("h2", {}, "I am a h2 tag")
+        ])
+    ]
+);
+
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent) // object
+
+//React.createElement(TagName,attributes,children)
+
+root.render(parent);
