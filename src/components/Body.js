cd227f5b7e0fbe83fe8c6d3a7dd8cc0b4de8b40a
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-11-03 14:28:16 +0530

    Episode 05 part 2

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import resList from "../utils/mockData";
 import ResrtrurantContainer from "./ResrtrurantCard";
 
 const Body = () => {
+  const [resListOfResturant, setResListOfResturant] = useState(resList);
+  const updatedResListByTheirRatings = (resList) => {
+    const upadatedReslist = resList.filter((res) => res.rating > 4);
+    setResListOfResturant(upadatedReslist);
+  };
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            updatedResListByTheirRatings(resList);
+          }}
+        >
+          Top Rated Resrtrurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList?.map((resDataObj) => (
+        {resListOfResturant?.map((resDataObj) => (
           <ResrtrurantContainer key={resDataObj?.id} resData={resDataObj} />
         ))}
       </div>
--- src/components/ResrtrurantCard.js
+++ src/components/ResrtrurantCard.js
@@ -2,23 +2,23 @@ import { CDN_URL, CDN_URL_COPY } from "../utils/constants";
 const ResrtrurantContainer = (props) => {
   const { name, cuisine, rating, ...otherProps } = props?.resData;
 
-  console.log(otherProps);
+  // console.log(otherProps);
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      {otherProps?.id === "000003" || otherProps?.id === "000004" ? (
+      {/* {otherProps?.id === "000003" || otherProps?.id === "000004" ? (
         <img
           className="res-logo"
           alt="res-card"
           src={`${CDN_URL_COPY}${otherProps.imgCode}`}
         />
-      ) : (
-        <img
-          className="res-logo"
-          alt="res-card"
-          src={`${CDN_URL}${otherProps.imgCode}`}
-        />
-      )}
+      ) : ( */}
+      <img
+        className="res-logo"
+        alt="res-card"
+        src={`${CDN_URL}${otherProps.imgCode}`}
+      />
+      {/* )} */}
       <h3>{name}</h3>
       <h4>{cuisine}</h4>
       <h4>{rating} star</h4>
