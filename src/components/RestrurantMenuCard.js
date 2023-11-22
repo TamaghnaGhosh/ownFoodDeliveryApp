ca3d254de24aaa0fccc6c914808e6a0e1663bb40
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-11-22 16:41:04 +0530

    cursor:pointer add on accordion header

--- src/components/RestrurantMenuCard.js
+++ src/components/RestrurantMenuCard.js
@@ -8,7 +8,7 @@ const RestrurantMenuCard = ({ item }) => {
     return (
         <div>
             {/* Header */}
-            <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
+            <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 cursor-pointer">
                 <div className="flex justify-between" onClick={() => handleClickOpenAccordion()}>
                     <span className='font-bold text-lg pl-3'>{item?.title} ({item?.itemCards?.length})</span>
                     <span>{showItems ? "🔽" : "🔼"}</span>
