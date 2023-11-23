dde2f2b8c678e1bd7731451e1537280ef9b9c9ef
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-11-23 09:47:00 +0530

    optional chaning

--- src/components/RestrurantItems/ItemMenuOfcards.js
+++ src/components/RestrurantItems/ItemMenuOfcards.js
@@ -4,7 +4,7 @@ import { MENU_API_CDN_IMG } from '../../utils/constants'
 const ItemMenuOfcards = ({ items }) => {
     return (
         <div>
-            {items.map((item) => (
+            {items?.map((item) => (
                 <div key={item?.card?.info?.id}
                     className='p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between'>
 
