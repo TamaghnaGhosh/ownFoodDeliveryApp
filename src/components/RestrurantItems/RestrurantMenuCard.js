0ac77705a685810dd0e6ffe0dd3d4fbae0c3ca16
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-11-24 11:00:59 +0530

    I have implemented this condition(item.itemCards.length > 0)

--- src/components/RestrurantItems/RestrurantMenuCard.js
+++ src/components/RestrurantItems/RestrurantMenuCard.js
@@ -5,18 +5,20 @@ const RestrurantMenuCard = ({ item }) => {
     const handleClickOpenAccordion = () => {
         setShowItems(item.itemCards.length > 0 ? !showItems : false);
     }
-    console.log(item.itemCards.length > 0);
+    // console.log(item.itemCards.length > 0);
     return (
         <div>
             {/* Header */}
-            <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 cursor-pointer">
-                <div className="flex justify-between" onClick={() => handleClickOpenAccordion()}>
-                    <span className='font-bold text-lg pl-3'>{item?.title} ({item?.itemCards?.length})</span>
-                    <span>{showItems ? "🔽" : "🔼"}</span>
+            {item.itemCards.length > 0 &&
+                <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 cursor-pointer">
+                    <div className="flex justify-between" onClick={() => handleClickOpenAccordion()}>
+                        <span className='font-bold text-lg pl-3'>{item?.title} ({item?.itemCards?.length})</span>
+                        <span>{showItems ? "🔽" : "🔼"}</span>
+                    </div>
+                    {/* Accordion Body */}
+                    {showItems && <ItemMenuOfcards items={item?.itemCards} />}
                 </div>
-                {/* Accordion Body */}
-                {showItems && <ItemMenuOfcards items={item?.itemCards} />}
-            </div>
+            }
         </div>
     )
 }
