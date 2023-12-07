f48583cf495ae58e35a33e351cb590aba71e67e9
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-12-07 15:21:53 +0530

    Episode 13 ( 2:41:10)

--- src/components/__tests__/ResrtrurantCard.test.js
+++ src/components/__tests__/ResrtrurantCard.test.js
@@ -1,7 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import ResrtrurantCard from '../ResrtrurantCard';
+import "@testing-library/jest-dom"
+import ResrtrurantCard, { WithLabelTopRatedRestrurants } from '../ResrtrurantCard';
+import MockData from "../../utils/MockData.json"
 
 it('should resturant card component with props data', () => {
-    render(<ResrtrurantCard  resData=""/>)
-    // screen
+    render(<ResrtrurantCard resData={MockData} />)
+    const name = screen.getByText('Andhra Gunpowder')
+    expect(name).toBeInTheDocument()
+});
+
+it('should render resturant card component with Popular Restaurant  label', () => {
+    const TopRatedHigherOrderComponents = WithLabelTopRatedRestrurants(ResrtrurantCard);
+    render(<TopRatedHigherOrderComponents resData={MockData} />)
+    const name = screen.getByText('Andhra Gunpowder')
+    expect(name).toBeInTheDocument()
 })
